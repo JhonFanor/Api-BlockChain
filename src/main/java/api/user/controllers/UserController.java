704a9de6cde18a8d6090dc9c27e7a4973089989b
JhonFanor <jforjuela@unillanos>
2024-09-23 08:45:22 -0500

    Arreglo

--- src/main/java/api/user/controllers/UserController.java
+++ src/main/java/api/user/controllers/UserController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,7 @@ import api.user.entities.UserEntitie;
 import api.user.services.UserService;
 
 @RestController
-@RequestMapping(value = "/api/notification")
+@RequestMapping(value = "/Api-BlockChain/user")
 public class UserController {
 
     private UserService userService;
@@ -40,12 +42,12 @@ public class UserController {
         return ResponseEntity.ok(userService.get(id).get());
     }
 
-    @PostMapping(path = "/update/{id}")
+    @PutMapping(path = "/update/{id}")
     public ResponseEntity<UserEntitie> update(@RequestBody UserEntitie user, @PathVariable Long id){
         return ResponseEntity.ok(userService.update(user,id)); 
     }
 
-    @GetMapping(path = "/get/{id}")
+    @DeleteMapping(path = "/delete/{id}")
     public ResponseEntity<UserEntitie> delete(@PathVariable Long id){
         return ResponseEntity.ok(userService.delete(id));
     }
